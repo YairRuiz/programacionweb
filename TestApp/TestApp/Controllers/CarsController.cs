bb9e3f4368c9bb9f528a9b289bcbe6e311613840
Yair Ruiz <Yairruiz79@gmail.com>
2020-11-03 14:13:05 -0800

    Practica 5

---
+++ TestApp/TestApp/Controllers/CarsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TestApp.Models;
+using TestApp.Library.DAL.Models;
+
+namespace TestApp.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly TestAppEntities _ctx;
+
+        public CarsController(TestAppEntities ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //_ctx.Persons.Add(new Persons() { email = "jesusriosdev@gmail.com", first_names = "Jesus", last_names = "Rios" });
+            //_ctx.Persons.Add(new Persons() { email = "ivan.cebreros@gmail.com", first_names = "Ivan", last_names = "Cebreros" });
+            //_ctx.SaveChanges();
+
+            var model = _ctx.Cars_Yair.ToList();
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var model = new Cars_Yair();
+            return View(model);
+
+        }
+
+        [HttpPost]
+        public IActionResult Add(Cars_Yair modelCar)
+        {
+            // Trabajar y agregar el model.
+
+            _ctx.Cars_Yair.Add(modelCar);
+            _ctx.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
---
+++ TestApp/TestApp/Models/CarViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Models
+{
+    public class CarViewModel
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+    }
+}
