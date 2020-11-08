8020d5130232f328d69431d1b518560b867e74d6
Yair Ruiz <Yairruiz79@gmail.com>
2020-11-08 13:33:25 -0800

    Ejercicio6

---
+++ Ejercicio6/TestApp/TestApp/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Hour { get; set; }
+    }
+}
---
+++ Ejercicio6/TestApp/TestApp/Models/SessionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Models
+{
+    public class SessionViewModel
+    {
+        public int UserId { get; set; }
+        public string FirstNames { get; set; }
+        public string LastNames { get; set; }
+        public int Hour = DateTime.Now.Hour;
+    }
+}
